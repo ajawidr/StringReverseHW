58d448c9af8e48c180d0882eda080fcd41c73a56
Jawid Rashidee <113652569+ajawidr@users.noreply.github.com>
2022-10-05 22:53:02 -0700

    Add files via upload

---
+++ StringReverse.java
@@ -0,0 +1,18 @@
+package homeWork;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+
+		String s="Herat";
+		System.out.println(s);
+		
+		for (int i= s.length()-1; i>=0; i--)
+		{
+			System.out.print(s.charAt(i));
+		}
+			
+
+	}
+
+}
